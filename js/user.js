adce7637816331f98c1e023ac1aa0e4cb03680b3
Julia Hofmacher <1910653219@stud.fh-kufstein.ac.at>
2020-03-19 19:12:29 +0100

    Aufgabe 2

--- js/user.js
+++ js/user.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
     var userListBody = $('.userList tbody');
+    var numberUser = 1;
 
     //@todo store and somehow update the current number of users
 
-
-    $('.needs-validation').submit(function(event) {
+    let submit = $('.needs-validation').submit(function(event) {
 
         event.preventDefault();
         event.stopPropagation();
@@ -20,27 +20,45 @@ $(document).ready(function() {
         //@todo
         //1. get values
         //2. create a new element
-        //3. somehow add them to userListBody
-        //4. update number of current users
-        //5. clear entries from the form
-        //6. maybe do something else... :-)
+        var eingabe = document.getElementById("username");
+        var newUser = eingabe.value; // Wert der neuen Eingabe in newUser speichern
 
-        //your code follows here
+        //3. somehow add them to userListBody
+        userListBody.push(newUser);
+
+        numberUser++;
+        console.log(numberUser);
+
+        //4. update number of current users
+        var length = userListBody.length;
+        var table = document.getElementById('table');
+        var newRow = table.insertRow(-1);
+        var newCell1 = newRow.insertCell(0);
+        var newCell2 = newRow.insertCell(1);
+        var newCell3 = newRow.insertCell(2);
+
+        newCell1.innerHTML = length;
+        newCell2.innerHTML = newUser;
+        newCell3.innerHTML = "<button type=\"button\" class=\"btn btn-secondary btn-danger deleteTrigger\" title=\"Löschen\"><i class=\"fa fa-trash\"></i></button>";
+        document.getElementById("form").reset();
+
+        //5. clear entries from the form
+
 
         return false;
     });
 
 
-    $('.deleteTrigger').click(function() {
+    $(document).on('click','.deleteTrigger',function() {
         //@todo
+
         //1. remove current user from dom
+        $(this).parents("tr").remove();
+
         //2. update number of current users
+        numberUser-- ;
+        console.log(numberUser);
 
-        //your code follows here
-
+        });
 
     });
-
-    //maybe some code follows here
-
-});
